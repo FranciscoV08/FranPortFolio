7efd5f5731e0e7006ff4f9bbd1d13beb8c135e5d
Francisco <francisco08@gmail.com>
2024-09-13 19:25:33 -0300

    05

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import Image from "next/image";
+// import Image from "next/image";
 import { useRouter } from "next/navigation";
 // import { useRouter } from "next/router";
 import { useEffect } from "react";
